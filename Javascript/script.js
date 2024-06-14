73090aea333455bd744d5665e664887276d293a7
kinza-khan <kinzaaptech@gmail.com>
2024-06-14 09:58:45 +0500

    js param function

---
+++ Javascript/script.js
@@ -0,0 +1,21 @@
+// let num1 = parseInt(prompt("Enter Your Number 1"));
+// let num2 = parseInt(prompt("Enter Your Number 2 "));
+// let addRes = num1+num2;
+// let subResult = num1 - num2 ;
+// let modResult = num1%num2;
+// document.write(num1 + " type is " + typeof(num1));
+// document.write('<br>');
+// document.write(num2 + " type is " + typeof(num2) + "<br>" + " Result of Addition is "+ addRes + "<br>" + " subtraction Result " + subResult + " <br>" + " modulus is " + modResult);
+// Relational Or Comparsion Operators
+//  let num1 = parseInt(prompt("Enter Your Number 1"));
+//  let num2 = parseInt(prompt("Enter Your Number 2 "));
+let num1 = 22;
+let num2 = 22;
+ let comp = (num1==num2);
+ let notEqual = (num1!=num2) 
+ let grateValue = num1>num2;
+ let lessValue = num1<num2;
+ let strictEqual = num1 === num2;
+ let notstrictEqual = (num1 !== num2);
+//  document.write( " Equal "+comp + "<br> not equal " + notEqual);
+ document.write(notstrictEqual);
